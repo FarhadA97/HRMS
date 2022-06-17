d030a43410b10562317e02e6a2f4e20ddfdecbf6
farhad <afarhad863@gmail.com>
2022-06-17 16:33:18 +0500

    refactored loginHandler

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -8,30 +8,20 @@ import { useNavigate } from "react-router-dom";
 const AuthPage = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const loginHandler: (isLogin: boolean, data: User) => void = (
-    isLogin,
-    data
-  ) => {
-    if (isLogin) {
-      dispatch(login({ data, url: loginURL }))
-        .unwrap()
-        .then(() => {
-          navigate("/");
-        })
-        .catch((e) => {
-          //empty for now
-        });
-    } else {
-      dispatch(login({ data, url: registerURL }))
-        .unwrap()
-        .then(() => {
-          navigate("/");
-        })
-        .catch((e) => {
-          //empty for now
-        });
-    }
+  const loginHandler: (isLogin: boolean, data: User) => void = (isLogin,data) => {
+    let url: string = loginURL;
+    if (!isLogin) url = registerURL;
+    dispatch(login({ data, url }))
+      .unwrap()
+      .then(() => {
+        navigate("/");
+      })
+      .catch((e) => {
+        // can dispatch toast either here or in catch block of the above dispatched login method which is async thunk function
+        // error toast is dispatched in catch block of asyncthunk login function
+      });
   };
+
   return <AuthForm onLogin={loginHandler} />;
 };
 
