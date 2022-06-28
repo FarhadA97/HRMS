5e89200804dad0feaa62c2968349030f51bc93b5
farhad <afarhad863@gmail.com>
2022-06-28 16:57:44 +0500

    minor fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,21 @@ import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import Toast from "./components/UI/Toast/Toast";
-import {useAppSelector } from "./store/hook";
+import {useAppDispatch, useAppSelector } from "./store/hook";
 import AddCandidatePage from "./pages/AddCandidatePage";
 import CandidatesPage from "./pages/CandidatesPage";
+import { getCandidates } from "./store/slices/candidate/CandidateActions";
+import { useEffect } from "react";
 
 const App = () => {
   const isLoggedIn:boolean = useAppSelector((state) => state.auth.isLoggedIn);
-  //const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
  
   //const isLoggedIn:boolean = localStorage.getItem("user") ? true : false;
   
-  
+  useEffect(() => {
+    dispatch(getCandidates());
+  }, [dispatch]);
 
   return (
     <>
@@ -23,6 +27,7 @@ const App = () => {
           <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate to='/login' replace/>} />
           <Route path="/candidates" element={isLoggedIn ? <CandidatesPage/> : <Navigate to='/login' replace />} />
           <Route path="/add" element={isLoggedIn ? <AddCandidatePage /> : <Navigate to='/login' replace />} />
+          <Route path="/edit/:id" element={isLoggedIn ? <AddCandidatePage /> : <Navigate to='/login' replace/>} />
           <Route path="/login" element={!isLoggedIn ? <AuthPage /> : <Navigate to='/' replace/>} />
           <Route path="*" element={isLoggedIn ? <Navigate to='/'/> : <Navigate to='/login' replace/>} />
         </Routes>
