d9d0d4b851a00334ac6c1b4c978143d44f40ecf0
farhad <afarhad863@gmail.com>
2022-06-30 17:40:33 +0500

    added notes module in candidateItem component

--- client/src/components/Candidate/Candidates/CandidateItem.tsx
+++ client/src/components/Candidate/Candidates/CandidateItem.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { useAppDispatch, useAppSelector } from "../../../store/hook";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { INotes } from "../../../config";
+import { useAppDispatch } from "../../../store/hook";
 import {deleteCandidate } from "../../../store/slices/candidate/CandidateActions";
 import Button from "../../UI/Button";
+import Notes from "../Notes/Notes";
 import classes from './CandidateItem.module.css'
+
+
+
 interface candidateData {
   name: string;
   email: string;
@@ -11,6 +17,7 @@ interface candidateData {
   field: string;
   reference: string;
   status?: string;
+  notes? : INotes[];
   _id?: string;
 }
 
@@ -19,12 +26,19 @@ interface Props {
 }
 
 const CandidateItem: React.FC<Props> = (props) => {
-  const isLoading = useAppSelector(state => state.candidate.loading);
+  const [notesId, setNotesId] = useState<string | null>(null);
   const dispatch = useAppDispatch();
-  
-  if(isLoading){
-    return (<h2 style={{textAlign : 'center'}}>Loading...</h2>)
+  const navigate = useNavigate();
+
+  const notesHandler = (id:string) => {
+      setNotesId((prevState) => 
+        prevState = prevState === id ? null : id
+      )
   }
+
+
+
+  
   if(props.list.length === 0){
     return (<h2 style={{textAlign : 'center'}}>There are no candidates.</h2>)
   }
@@ -55,8 +69,10 @@ const CandidateItem: React.FC<Props> = (props) => {
               <th>{item.status}</th>
               <th>
                 <div className={classes.btn}>
-                  <Button className="actions">Edit</Button>
+                  <Button className="actions" onClick={() => navigate(`/edit/${item._id}`)}>Edit</Button>
                   <Button className="actions" onClick={() => {dispatch(deleteCandidate({id:item._id ? item._id : ''}))}}>Delete</Button> 
+                  <Button className="actions" onClick={notesHandler.bind(null,item._id!)} > {notesId === item._id ? 'Hide Notes' : 'View Notes'} </Button>
+                  {notesId === item._id && <Notes candidateId={item._id} onCancel = {notesHandler} />}
                 </div>
                 
               </th>
